29aa291941c392c229f6ea09d59957eadd934af6
Yustyn <yustyn23@gmail.com>
2021-11-18 15:19:39 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,139 @@
+import unittest
+import re
+from methods import Admin, SuperAdmin
+from connection import Connection
+from validation import Validate, USERS
+
+
+class SuperAdminTests(unittest.TestCase):
+    # valid data
+    VALID_EMAIL = 'Correct@email.com'
+    VALID_PASSWORD = 'AQwe12!_'
+
+    ADMIN_DATA = [{
+        "first_name": "Bob",
+        "last_name": "Bobb",
+        "date_of_bitrth": "2-5-1684",
+        "phone": "+380123456789",
+        "address": "Some  15 st. 17 app.",
+        "password": "Qwewe123!3",
+        "email": "opa@mail.dog",
+        "role": "admin",
+        "discount": 20
+    }]
+
+    INVALID_ADMIN_DATA = [{
+        "first_name": "Bill",
+        "last_name": "Bobb",
+        "date_of_bitrth": "02.05.1684",
+        # "phone":"+803254",
+        # "address":"Streee1",
+        # "password":"123",
+        "email": "opa@mail.dog",
+        "role": "someone",
+        "discount": "20"
+    }]
+
+    # invalid data
+    INVALID_EMAIL = 'inorrect@@email.com'
+    INVALID_PASSWORD = 12345678
+
+    def setUp(self):
+        # create SuperAdmin
+        self.super_admin = SuperAdmin(self.VALID_EMAIL, self.VALID_PASSWORD)
+        # return super().setUp()
+        pass
+
+    def tearDown(self):
+        selector = Connection().getNextId('users')-1
+        self.super_admin.delete_admin(selector)
+        return super().tearDown()
+
+    def clear_record(self, table):
+        selector = Connection().getNextId(table)-1
+        self.super_admin.delete_admin(selector)
+
+    # def validate_fields(self, request, model, model_name):
+    #     for
+
+    def test_create_SuperAdmin(self):
+        super_admin_val = SuperAdmin(self.VALID_EMAIL, self.VALID_PASSWORD)
+        self.assertIsInstance(super_admin_val, SuperAdmin)
+        print('Test 1.1: pass.')
+
+    def test_create_invalid_SuperAdmin(self):
+        super_admin_inv = SuperAdmin(self.INVALID_EMAIL, self.INVALID_PASSWORD)
+        # self.assertRaises(ValueError, msg='Incorrect email!')
+        email_pattern = re.compile(
+            r'^([a-zA-Z0-9-_\*\.]+)@([a-zA-Z0-9-]+)(\.[a-zA-Z0-9]+)+$')
+
+        self.assertNotRegex(self.INVALID_EMAIL,
+                            email_pattern, 'Incorrect email!')
+        self.assertNotIsInstance(self.INVALID_PASSWORD, str,
+                                 f'Incorect data type! It must been str but returned {type(self.INVALID_PASSWORD)}')
+        self.assertIsInstance(super_admin_inv, SuperAdmin)
+        print('Test 1.2: pass.')
+
+    def test_add_admin(self):
+        Validate().validate(self.ADMIN_DATA[0], USERS, 'Users')
+        response = self.super_admin.add_admin(self.ADMIN_DATA)
+        self.assertEqual(response, 'Insert done!')
+        print('Test 1.3: pass.')
+        # self.clear_record('users')
+
+    def test_add_invalid_admin(self):
+        Validate().validate(self.INVALID_ADMIN_DATA[0], USERS, 'Users')
+        response = self.super_admin.add_admin(self.INVALID_ADMIN_DATA)
+        self.assertEqual(response, 'Insert done!')
+        print('Test 1.3: pass.')
+        # self.clear_record('users')
+
+
+class AdminTests(unittest.TestCase):
+    VALID_EMAIL = 'Correct@email.com'
+    VALID_PASSWORD = 'AQwe12!_'
+
+    ADMIN_DATA = [{
+        "first_name": "Bob",
+        "last_name": "Bobb",
+        "date_of_bitrth": "2-5-1684",
+        "phone": "+380123456789",
+        "address": "Some  15 st. 17 app.",
+        "password": "Qwewe123!3",
+        "email": "opa@mail.dog",
+        "role": "admin",
+        "discount": 20
+    }]
+
+    def setUp(self):
+        # create SuperAdmin
+        self.cmn_admin = Admin(self.VALID_EMAIL, self.VALID_PASSWORD)
+        # return super().setUp()
+        pass
+
+    def test_create_CmnAdmin(self):
+        cmn_admin_val = Admin(self.VALID_EMAIL, self.VALID_PASSWORD)
+        self.assertIsInstance(cmn_admin_val, Admin)
+        print('Test 1.1: pass.')
+
+    def test_add_admin(self):
+        Validate().validate(self.ADMIN_DATA[0], USERS, 'Users')
+        response = self.cmn_admin.add_admin(self.ADMIN_DATA)
+        self.assertEqual(response, 'Insert done!')
+        print('Test 1.3: pass.')
+        # self.clear_record('users')
+
+    def test_create_valid_Admin(self):
+        cmn_admin_inv = Admin(self.VALID_EMAIL, self.VALID_PASSWORD)
+        # self.assertRaises(ValueError, msg='Incorrect email!')
+        email_pattern = re.compile(
+            r'^([a-zA-Z0-9-_\*\.]+)@([a-zA-Z0-9-]+)(\.[a-zA-Z0-9]+)+$')
+
+        self.assertRegex(self.VALID_EMAIL, email_pattern, 'correct email!')
+        print('Test 1.2: pass.')
+    pass
+
+    # def test_add_admin(self):
+    #     admin_1 = SuperAdmin('Bad','BOB').add_admin(admin_1_data)
+if __name__ == '__main__':
+    unittest.main()
